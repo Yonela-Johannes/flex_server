fb8716cd0a6cd9551cc19903cf9259e8c0d4d7e2
Yonela Johannes <johannesyonela@gmail.com>
2024-02-15 01:45:35 +0200

    Init: client side/frontend

--- client/test.py
+++ client/test.py
@@ -6,13 +6,18 @@ c1 = Client("Yonela")
 c2 = Client("Fannie")
 
 def update_messages():
+    """
+    Updates the local list of messages
+    :return: None
+    """
     msgs = []
     run = True
     while run:
-        time.sleep(0.5)
-        new_messages = c1.get_messages()
-        msgs.extend(new_messages)
-        for msg in new_messages:
+        time.sleep(0.1) #update every 1/10 of a second
+        new_messages = c1.get_messages() # get any new message from client
+        msgs.extend(new_messages) # add to local list of messages
+
+        for msg in new_messages: # display new messages
             print(msg)
             if msg == "{quit}":
                 run = False
